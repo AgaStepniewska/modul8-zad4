b9f309029de8293e9ff703da2da77e45261ba361
pinko <agaslota@wp.pl>
2017-05-31 23:20:49 +0100

    operation triangle

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+var a = 5; // triangle side
+	h = 8; // Triangle height
+	triangleArea = [a*h/2];
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);	
