5166d1612d91af7626ca8f13f1dec5d1f6e702e1
pinko <agaslota@wp.pl>
2017-05-31 23:54:07 +0100

    I used prompt and added alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // scripts.js
-var a = 5; // triangle side
-	h = 8; // Triangle height
+var a = prompt('podaj wartość dla podstawy trójkąta');// triangle side
+	h = prompt('podaj wysokość trójkąta'); // Triangle height
 	triangleArea = [a*h/2];
+alert('Pole trójkąta wynosi:  ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);	
