60e5bf1ebfa8569148efd4e281a8fd4a225b49e2
pinko <agaslota@wp.pl>
2017-06-01 20:21:32 +0100

    little corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // scripts.js
-var a = prompt('podaj wartość dla podstawy trójkąta');// triangle side
-	h = prompt('podaj wysokość trójkąta'); // Triangle height
-	triangleArea = [a*h/2];
+var a = prompt('podaj wartość dla podstawy trójkąta'),// triangle side
+	h = prompt('podaj wysokość trójkąta'), // Triangle height
+	triangleArea = a*h/2;
 alert('Pole trójkąta wynosi:  ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);	
